7bdb405394c62666c2b0f41f9f6f9d8719de75d1
Luis Gaspari <luisgaspari@gmail.com>
2024-08-23 12:43:23 +0000

    feat: estilização com Gluestack-ui

--- app/index.tsx
+++ app/index.tsx
@@ -3,29 +3,39 @@ import { StyleSheet, Text, View } from 'react-native';
 import StyledInput from '../components/styled-input';
 import StyledButton from '../components/styled-button';
 import { router } from 'expo-router';
+import { Heading } from '@/components/ui/heading';
+import { Button, ButtonText } from '@/components/ui/button';
 
 export default function Login() {
 
-    const handleLogin = () => {
-        router.replace('home')
-    }
+  const handleLogin = () => {
+    router.replace('home')
+  }
 
   return (
     <View style={styles.container}>
-      <Text>Login</Text>
-      <StyledInput 
-      placeholder='Digite seu login' 
-      onChangeText={(texto) => console.log(texto)}
+      {/* <Text>Login</Text> */}
+
+      <Heading size='3xl'>Login</Heading>
+
+      <StyledInput
+        placeholder='Digite seu login'
+        onChangeText={(texto) => console.log(texto)}
       />
-      <StyledInput 
-      placeholder='Digite sua senha' 
-      onChangeText={(texto) => console.log(texto)}
+      <StyledInput
+        placeholder='Digite sua senha'
+        onChangeText={(texto) => console.log(texto)}
       />
-      <StyledButton 
-      text='Entrar' 
-      onClick={handleLogin}
-      color='blue' 
-      />
+      {/* <StyledButton
+        text='Entrar'
+        onClick={handleLogin}
+        color='blue'
+      /> */}
+
+      <Button onPress={handleLogin} size="lg" variant="solid" >
+        <ButtonText>Entrar</ButtonText>
+      </Button>
+
       <StatusBar style="auto" />
     </View>
   );
